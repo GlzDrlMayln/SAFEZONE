bec857847029a5515868215f291f849143f2e930
GlzDrlMayln <darlenemaylan04@gmail.com>
2024-12-22 23:21:22 +0800

    register(unfinished)

--- lib/app_routes.dart
+++ lib/app_routes.dart
@@ -1,4 +1,5 @@
 import 'package:go_router/go_router.dart';
+import 'package:safezone/frontend/pages/authentication/register.dart';
 import 'package:safezone/frontend/pages/main-screen/report-danger-zone/create_report.dart';
 import 'package:safezone/frontend/pages/main-screen/report-danger-zone/report_success.dart';
 import 'package:safezone/frontend/pages/main-screen/report-danger-zone/reports_history.dart';
@@ -10,33 +11,40 @@ import 'package:safezone/frontend/pages/main-screen/sos/sos_countdown.dart';
 import 'package:safezone/frontend/pages/main-screen/sos/sos_success.dart';
 import 'package:safezone/frontend/widgets/bottom_navigation.dart';
 
-final GoRouter appRouter = GoRouter(routes: [
-  GoRoute(
-    path: '/',
-    builder: (context, state) => BottomNavigationWidget(),
-  ),
-  GoRoute(
-      path: '/create-report',
-      builder: (context, state) => const CreateReport()),
-  GoRoute(
-      path: '/review-report',
-      builder: (context, state) => const ReviewReport()),
-  GoRoute(
-      path: '/report-success',
-      builder: (context, state) => const ReportSuccess()),
-  GoRoute(
-      path: '/reports-history',
-      builder: (context, state) => const ReportsHistory()),
-  GoRoute(
-      path: '/mark-safe-zone',
-      builder: (context, state) => const MarkSafeZone()),
-  GoRoute(
-      path: '/mark-safe-zone-success',
-      builder: (context, state) => const MarkSafeSuccess()),
-  GoRoute(path: '/sos-page', builder: (context, state) => const SosPage()),
-  GoRoute(
-      path: '/sos-countdown',
-      builder: (context, state) => const SosCountdown()),
-  GoRoute(
-      path: '/sos-success', builder: (context, state) => const SosSuccess()),
-]);
+final GoRouter appRouter = GoRouter(
+  initialLocation: '/register', 
+  routes: [
+    GoRoute(
+      path: '/register',
+      builder: (context, state) => RegisterScreen(),
+    ),
+    GoRoute(
+      path: '/',
+      builder: (context, state) => BottomNavigationWidget(),
+    ),
+    GoRoute(
+        path: '/create-report',
+        builder: (context, state) => const CreateReport()),
+    GoRoute(
+        path: '/review-report',
+        builder: (context, state) => const ReviewReport()),
+    GoRoute(
+        path: '/report-success',
+        builder: (context, state) => const ReportSuccess()),
+    GoRoute(
+        path: '/reports-history',
+        builder: (context, state) => const ReportsHistory()),
+    GoRoute(
+        path: '/mark-safe-zone',
+        builder: (context, state) => const MarkSafeZone()),
+    GoRoute(
+        path: '/mark-safe-zone-success',
+        builder: (context, state) => const MarkSafeSuccess()),
+    GoRoute(path: '/sos-page', builder: (context, state) => const SosPage()),
+    GoRoute(
+        path: '/sos-countdown',
+        builder: (context, state) => const SosCountdown()),
+    GoRoute(
+        path: '/sos-success', builder: (context, state) => const SosSuccess()),
+  ],
+);
