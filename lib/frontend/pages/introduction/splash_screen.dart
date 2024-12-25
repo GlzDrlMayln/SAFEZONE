d43a9c451c72dc6c92c317205737e34c2512deec
GlzDrlMayln <darlenemaylan04@gmail.com>
2024-12-25 21:04:59 +0800

    splashscreen

--- lib/frontend/pages/introduction/splash_screen.dart
+++ lib/frontend/pages/introduction/splash_screen.dart
@@ -1,145 +1,65 @@
-// ignore_for_file: prefer_const_constructors
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:safezone/frontend/pages/introduction/onboarding_screen.dart';
 
 class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
   @override
-  _SplashScreenState createState() => _SplashScreenState();
+  State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
-  final List<OnboardingContent> contents = [
-    OnboardingContent(
-      image: 'assets/female.svg',
-      title: 'Women Safety',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-    ),
-    OnboardingContent(
-      image: 'assets/alertsos.svg',
-      title: 'Emergency Help',
-      description: 'Stay safe with our SOS alert features.',
-    ),
-       OnboardingContent(
-      image: 'assets/logo.svg',
-      title: 'lorem ipsum',
-      description: 'Stay safe with our SOS alert features.',
-    ),
-  ];
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin {
+  @override
+  void initState() {
+    super.initState();
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
+
+    Future.delayed(Duration(seconds: 2), () {
+      Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: (_) => OnboardingScreen()));
+    });
+  }
+
+  @override
+  void dispose() {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+        overlays: SystemUiOverlay.values);
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Expanded(
-            child: PageView.builder(
-              itemCount: contents.length,
-              itemBuilder: (_, i) {
-                return Padding(
-                  padding: const EdgeInsets.all(40),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      SvgPicture.asset(
-                        contents[i].image,
-                        height: 300,
-                      ),
-                      SizedBox(height: 20),
-                      Text(
-                        contents[i].title,
-                        style: TextStyle(
-                          color: const Color.fromRGBO(239, 141, 136, 1),
-                          fontSize: 20,
-                          fontFamily: "Poppins",
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
-                      SizedBox(height: 13),
-                      Text(
-                        contents[i].description,
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          fontSize: 15,
-                          fontWeight: FontWeight.w200,
-                          color: const Color(0xFF707070),
-                        ),
-                      ),
-                    ],
-                  ),
-                );
-              },
+      body: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/images/logo.jpg',
+              width: 150,
+              height: 150,
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Expanded(
-                  child: TextButton(
-                    onPressed: () {
-                      // Skip button logic here
-                    },
-                    style: TextButton.styleFrom(
-                      backgroundColor: Colors.transparent,
-                      padding: const EdgeInsets.symmetric(vertical: 13),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
-                        side: const BorderSide(
-                          color: Color(0xFFEF8D88),
-                        ),
-                      ),
-                    ),
-                    child: Text(
-                      'Skip',
-                      style: GoogleFonts.poppins(
-                        fontSize: 15,
-                        color: const Color(0xFFEF8D88),
-                      ),
-                    ),
-                  ),
-                ),
-                SizedBox(width: 8),
-                Expanded(
-                  child: TextButton(
-                    onPressed: () {
-                      // Next button logic here
-                    },
-                    style: TextButton.styleFrom(
-                      backgroundColor: const Color(0xFFEF8D88),
-                      padding: const EdgeInsets.symmetric(vertical: 13),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                    ),
-                    child: Text(
-                      'Next',
-                      style: GoogleFonts.poppins(
-                        fontSize: 15,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
+            SizedBox(height: 37),
+            Text(
+              'SafeZone',
+              style: GoogleFonts.poppins(
+                fontSize: 20,
+                fontWeight: FontWeight.w500,
+                color: const Color(0xFFEF8D88),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
-}
-
-class OnboardingContent {
-  final String image;
-  final String title;
-  final String description;
-
-  OnboardingContent({
-    required this.image,
-    required this.title,
-    required this.description,
-  });
 }
